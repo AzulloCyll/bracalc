e08e6e695d78c41de984465a5ff41dbd11544a8d
Daniel Chmur <daniel.chmur@gmail.com>
2024-03-02 14:29:06 +0100

    chore

--- bracalc.js
+++ bracalc.js
@@ -14,6 +14,8 @@ const {
   combineSizeName,
 } = require("./functions");
 
+const { sizes } = require("./sizes");
+
 const gorteks =
   "https://www.gorteks.com.pl/Kalkulator-rozmiaru-cabout-pol-138.html";
 
@@ -33,32 +35,6 @@ const detectBraSizeName = (underbust, bust) => {
 
 const detected = detectBraSizeName(70, 90);
 
-const sizes = [
-  //65
-  { name: "65B", number: 290 },
-  { name: "65C", number: 213 },
-  { name: "65D", number: 203 },
-  { name: "65E", number: 203 },
-  { name: "65F", number: 188 },
-  { name: "65G", number: 186 },
-  { name: "65H", number: 189 },
-  { name: "65I", number: 190 },
-  { name: "65J", number: 191 },
-  //70
-  { name: "70A", number: 224 },
-  { name: "70B", number: 181 },
-  { name: "70C", number: 182 },
-  { name: "70D", number: 161 },
-  { name: "70E", number: 162 },
-  { name: "70F", number: 163 },
-  { name: "70G", number: 192 },
-  { name: "70H", number: 193 },
-  { name: "70I", number: 194 },
-  { name: "70J", number: 195 },
-  { name: "70K", number: 278 },
-  { name: "70L", number: 279 },
-];
-
 const URL =
   "https://www.kontri.pl/pl/menu/biustonosze-100.html?filter_traits[12133]";
 
@@ -75,10 +51,11 @@ const getOverallURL = (detected, URL, sizes) => {
   }
 };
 
-getSingleURL = (size, URL, sizes) => {
+const getSingleURL = (size, URL, sizes) => {
   let url = (URL += "&filter_sizes=");
   const found = sizes.find((item) => item.name === size);
   return (url += `${found.number}`);
 };
 
-console.log(getOverallURL(detected, URL, sizes));
+const readSizes = getOverallURL(detected, URL, sizes);
+console.log(readSizes);
---
+++ sizes.js
@@ -0,0 +1,29 @@
+const sizes = [
+  //65
+  { name: "65B", number: 290 },
+  { name: "65C", number: 213 },
+  { name: "65D", number: 203 },
+  { name: "65E", number: 203 },
+  { name: "65F", number: 188 },
+  { name: "65G", number: 186 },
+  { name: "65H", number: 189 },
+  { name: "65I", number: 190 },
+  { name: "65J", number: 191 },
+  //70
+  { name: "70A", number: 224 },
+  { name: "70B", number: 181 },
+  { name: "70C", number: 182 },
+  { name: "70D", number: 161 },
+  { name: "70E", number: 162 },
+  { name: "70F", number: 163 },
+  { name: "70G", number: 192 },
+  { name: "70H", number: 193 },
+  { name: "70I", number: 194 },
+  { name: "70J", number: 195 },
+  { name: "70K", number: 278 },
+  { name: "70L", number: 279 },
+];
+
+module.exports = {
+  sizes,
+};
